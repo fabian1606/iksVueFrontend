d636d8ebf0ac41812db44c5efa53546af554b9de
fabian1606 <fabian@rafreider.com>
2024-05-28 20:18:08 +0200

    init

---
+++ src/components/pageDown.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Page Down</h1>
+    </div>
+  </template>
+
+<script>
+</script>
+<style>
+</style>
---
+++ src/components/pageUp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="up">
+        <h1>PageUp</h1>
+    </div>
+</template>
+<script>
+</script>
+<style scoped>
+    .up {
+        background-color: aliceblue;
+    }
+
+</style>
